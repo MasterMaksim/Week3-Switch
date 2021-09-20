5d7d9c669553519479ea8333d0458c9285cd51a8
Maksim Muravjov <mmuravjo@tktk.ee>
2021-09-20 21:31:48 +0300

    add new projects

---
+++ GradingIf/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GradingIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis hinde te saite?");
+            char userGrade = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if (userGrade == 'A')
+            {
+                Console.WriteLine("Suurepärane!");
+            }
+            else if (userGrade == 'B')
+            {
+                Console.WriteLine("Väga hea!");
+            }
+            else if (userGrade == 'C')
+            {
+                Console.WriteLine("Hea!");
+            }
+            else if (userGrade == 'D')
+            {
+                Console.WriteLine("Rahuldav!");
+            }
+            else if (userGrade == 'E')
+            {
+                Console.WriteLine("Kasin!");
+            }
+            else if (userGrade == 'F')
+            {
+                Console.WriteLine("Puudulik.");
+            }
+
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
